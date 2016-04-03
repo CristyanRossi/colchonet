e69766103dba003f136906375a7682d28920765c
CRISTYAN ROSSI <CRISTYANROSSI@GMAIL.com>
2016-04-03 18:06:24 -0300

    5.6 Reaproveitando as apresentações com partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def  update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user,
+				notice: 'Cadastro atualizado com sucesso !'
+		else
+			render action: :edit
+		end
+	end
+
 	private
 
 	def user_params
