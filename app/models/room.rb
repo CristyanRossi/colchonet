caa5f6c8283615c2f656c8461ca61bfb5acf023a
CRISTYAN ROSSI <CRISTYANROSSI@GMAIL.com>
2016-04-19 17:06:46 -0300

    10.6 Exibição e listagem de quartos

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ActiveRecord::Base
 	belongs_to :user
 	validates_presence_of :title, :location, :description
-
+	scope :most_recent, -> { order('created_at DESC') }
 	def complete_name
 		"#{title}, #{location}"
 	end
